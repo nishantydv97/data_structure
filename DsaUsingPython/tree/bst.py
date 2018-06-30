a5e38dff79e8b9b4fcbc71d267240847e826e2f5
nishantydv97 <phy.nishant@gmail.com>
2018-06-30 11:11:49 +0530

    python bst done

--- DsaUsingPython/tree/bst.py
+++ DsaUsingPython/tree/bst.py
@@ -52,6 +52,18 @@ class bst:
             self.preorder_display(dnode.left)
             self.preorder_display(dnode.right)
 
+
+    def bst_search(self,tree_root,data):
+        if(tree_root is None):
+            return(False)
+        elif(tree_root.data == data):
+            return (True)
+        elif(data<=tree_root.data):
+            return(self.bst_search(tree_root.left,data))
+        else:
+            return(self.bst_search(tree_root.right,data))
+
+
 def main():
     tree=bst()
     tr=tree.create_node(0)
@@ -61,16 +73,30 @@ def main():
     tree.insert_node(tr,25)
     tree.insert_node(tr,8)
     tree.insert_node(tr,12)
-#    print(tr.right.left.right.data)
+
     print("postorder traversal of the above tree is ")
     tree.post_display(tr)
     print("\n")
+
     print("preorder traversal of the above tree is ")
     tree.preorder_display(tr)
     print("\n")
+
     print("inorder traversal of the above tree is ")
     tree.inorder_display(tr)
     print("\n")
+
+    z=tree.bst_search(tr,12)
+    if(z):
+        print("number 12 found in the tree ")
+    else:
+        print("number 12 not found in the tree ")
+#    print("\n")
+    z=tree.bst_search(tr,145)
+    if(z):
+        print("number 145 found in the tree ")
+    else:
+        print("number 145 not found in the tree ")
 
 
 if __name__ == '__main__':
